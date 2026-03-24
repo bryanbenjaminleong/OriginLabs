8671b6dc0b7e3018552ba85ab6841a339aa881e6
bryanbenjaminleong <bryanbleong@gmail.com>
2026-03-24 23:01:19 +0800

    commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,9 +17,9 @@ export default function Header({ menuOpen = false, setMenuOpen }: HeaderProps) {
         <Logo />
       </div>
       <nav className={`nav-links ${menuOpen ? 'open' : ''}`}>
+        <a href="#research" className="nav-link" onClick={handleNavClick}>Research</a>
         <a href="#products" className="nav-link" onClick={handleNavClick}>Products</a>
         <a href="#vision" className="nav-link" onClick={handleNavClick}>Vision</a>
-        <a href="#research" className="nav-link" onClick={handleNavClick}>Research</a>
         <a href="#team" className="nav-link" onClick={handleNavClick}>Team</a>
       </nav>
       <a href="#contact" className="btn btn-outline header-cta">Get in Touch</a>
